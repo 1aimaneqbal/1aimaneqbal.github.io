f4af61f35c146c96c93a98b2786661d89042e88f
1aimaneqbal <1aimaneqbal@gmail.com>
2021-07-08 05:46:23 +0530

    Added Hash Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useState} from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import './styles/styles.scss';
 import Home from './components/main/Home'
@@ -7,16 +7,7 @@ import IllustratorComponent from './components/ai'
 import PreLoader from './components/PreLoader';
 function App() {
   const [theme, setTheme] = useState(false)
-  // const cursorRef = useRef(null)
-  // const cursorRef2 = useRef(null)
-  // const [cursorLoc, setCursorLoc] = useState([ ])
   useEffect(()=>{
-  //   document.addEventListener('mousemove', e =>{
-  //     cursorRef.current.style.top=`${e.pageY - 10}px`
-  //     cursorRef2.current.style.top=`${e.pageY - 10}px`
-  //     cursorRef.current.style.left=`${e.pageX - 10}px`
-  //     cursorRef2.current.style.left=`${e.pageX - 10}px`
-  //   })
   const thisWhat="security"
   // document.onkeydown = e =>{
   //   if(e.key==="F12"){
@@ -44,8 +35,6 @@ function App() {
           <IllustratorComponent theme={theme} setTheme={setTheme}/>
         </Route>
       </Router>
-      {/* <div className="cursor" ref={cursorRef} />
-      <div className="cursor2" ref={cursorRef2} /> */}
     </>
   );
 }
