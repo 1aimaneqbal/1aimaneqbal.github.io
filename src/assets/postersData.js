d650e1935bb0e17ce571ef57bcbffbdbde5ad23a
1aimaneqbal <1aimaneqbal@gmail.com>
2021-07-13 05:45:34 +0530

    Added Poster Hobby Section

---
+++ src/assets/postersData.js
@@ -0,0 +1,105 @@
+import img0 from "../images/photoshop/0.png"
+import img1 from "../images/photoshop/1.png"
+import img2 from "../images/photoshop/2.png"
+import img3 from "../images/photoshop/3.png"
+import img4 from "../images/photoshop/4.png"
+import img5 from "../images/photoshop/5.png"
+import img6 from "../images/photoshop/6.png"
+import img7 from "../images/photoshop/7.png"
+import img8 from "../images/photoshop/8.png"
+import img9 from "../images/photoshop/9.png"
+import img10 from "../images/photoshop/10.png"
+import img11 from "../images/photoshop/11.jpg"
+import img12 from "../images/photoshop/12.png"
+import img13 from "../images/photoshop/13.png"
+import img14 from "../images/photoshop/14.png"
+import img15 from "../images/photoshop/15.png"
+import img16 from "../images/photoshop/16.png"
+import img17 from "../images/photoshop/17.png"
+import img18 from "../images/photoshop/18.png"
+import img19 from "../images/photoshop/19.png"
+function postersData(){
+    return[
+        {
+            key: 0,
+            img: img0,
+        },
+        {
+            key: 1,
+            img: img1,
+        },
+        {
+            key: 2,
+            img: img2,
+        },
+        {
+            key: 3,
+            img: img3,
+        },
+        {
+            key: 4,
+            img: img4,
+        },
+        {
+            key: 5,
+            img: img5,
+        },
+        {
+            key: 6,
+            img: img6,
+        },
+        {
+            key: 7,
+            img: img7,
+        },
+        {
+            key: 8,
+            img: img8,
+        },
+        {
+            key: 9,
+            img: img9,
+        },
+        {
+            key: 10,
+            img: img10,
+        },
+        {
+            key: 11,
+            img: img11,
+        },
+        {
+            key: 12,
+            img: img12,
+        },
+        {
+            key: 13,
+            img: img13,
+        },
+        {
+            key: 14,
+            img: img14,
+        },
+        {
+            key: 15,
+            img: img15,
+        },
+        {
+            key: 16,
+            img: img16,
+        },
+        {
+            key: 17,
+            img: img17,
+        },
+        {
+            key: 18,
+            img: img18,
+        },
+        {
+            key: 19,
+            img: img19,
+        },
+    ]
+}
+export default postersData
--- src/components/ps.js
+++ src/components/ps.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../images/Logo.png'
 import {NavLink} from 'react-router-dom'
-import { UilMoon, UilSun, UilApps, UilTimes, UilEstate, UilBookOpen, UilUser, UilDesktop, UilCommentImage, UilSuitcaseAlt } from '@iconscout/react-unicons'
+import { UilMoon, UilSun, UilTimes } from '@iconscout/react-unicons'
+import posters from '../assets/postersData'
 
-const ps = ({theme, setTheme}) => {
+const Ps = ({theme, setTheme}) => {
+    const posterData = posters()
+    const [show, setShow] = useState(false)
+    const [tempImg, setTempImg] = useState('')
+    const showImg = (poster) => {
+        setTempImg(poster)
+        setShow(true)
+        console.warn(poster)
+    }
     const themeToggle = () =>{
         setTheme(!theme)
     }
     return (
-        <nav className={`${theme ? 'navDark' : ''}`}>
-            <div className="navLogo">
-                <NavLink to="/"><img src={Logo}/></NavLink>
-            </div>
-            PhotoShop
-            <div className="navToggle">
-                <div className='themeToggle' onClick={themeToggle}>
-                    {theme ? <UilSun /> : <UilMoon /> }
+        <>
+            <nav className={`${theme ? 'navDark' : ''}`}>
+                <div className="navLogo">
+                    <NavLink to="/"><img src={Logo}/></NavLink>
                 </div>
+                <h3 style={{color: "rgb(88, 80, 236)"}}>Posters</h3>
+                <div className="navToggle">
+                    <div className='themeToggle' onClick={themeToggle}>
+                        {theme ? <UilSun /> : <UilMoon /> }
+                    </div>
+                </div>
+            </nav>
+            <div className={show? "show open": "show"}>
+                <img src={tempImg} />
+                <UilTimes onClick={()=>setShow(false)} />
             </div>
-        </nav>
+            <div className={`posterContainer ${theme ? "darkThemeBG" : ''}`}>
+                {
+                    posterData.map(poster=>(
+                        <div key={poster.key} className="poster" onClick={()=>showImg(poster.img)}>
+                            <img src={poster.img} style={{width: '100%'}}/>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
     )
 }
 
-export default ps
+export default Ps
